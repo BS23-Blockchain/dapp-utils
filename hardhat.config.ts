3031bf5e30ed43d98c7fa194b873523ea7fe7bb8
Noman5237 <anonyman637@gmail.com>
2022-01-21 11:12:27 +0600

    strings library adapted for version 0.8.9

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,4 +1,3 @@
-import * as dotenv from "dotenv";
 import path from "path";
 import fs from "fs";
 import { execSync } from "child_process";
@@ -6,34 +5,26 @@ import { execSync } from "child_process";
 import { HardhatUserConfig, task } from "hardhat/config";
 import "@nomiclabs/hardhat-ethers";
 import "@nomiclabs/hardhat-waffle";
-import "@typechain/hardhat";
 import "hardhat-gas-reporter";
 import "solidity-coverage";
 
 import { TASK_COMPILE_SOLIDITY_GET_SOLC_BUILD } from "hardhat/builtin-tasks/task-names";
 import { TaskArguments } from "hardhat/types";
 
-dotenv.config();
-
 const installSolcJs = (version: string) => {
-  console.log(`installing correct solc version: ${version}`);
-  execSync(`npm i -D solc@${version}`, { stdio: [0, 1, 2] });
+  console.log(`installing solc@${version}`);
+  execSync(`npm i -D solc-${version}@npm:solc@${version}`, { stdio: [0, 1, 2] });
   execSync(`npm audit fix`, { stdio: [0, 1, 2] });
 };
 
 // Overriding the solidity compiler configuration task
 task(TASK_COMPILE_SOLIDITY_GET_SOLC_BUILD, async (args: TaskArguments, hre, runSuper) => {
   // check if solc with that version is installed
-  const solcPath = path.join(__dirname, "node_modules", "solc");
+  const solcPath = path.join(__dirname, "node_modules", `solc-${args.solcVersion}`);
   if (fs.existsSync(solcPath)) {
-    // if not installed update the solc version by running npm
-    const solc: { version: string } = JSON.parse(fs.readFileSync(path.join(solcPath, "package.json"), "utf8"));
-    console.log(`found solc version: ${solc.version}`);
-    if (solc.version !== args.solcVersion) {
-      installSolcJs(args.solcVersion);
-    }
+    console.log(`found solc@${args.solcVersion}`);
   } else {
-    console.log(`didn't found solc`);
+    console.log(`didn't found solc@${args.solcVersion}`);
     installSolcJs(args.solcVersion);
   }
 
@@ -46,26 +37,15 @@ task(TASK_COMPILE_SOLIDITY_GET_SOLC_BUILD, async (args: TaskArguments, hre, runS
   };
 });
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-const PROVIDER: string = process.env.PROVIDER || "";
-const NETWORKS = Object.fromEntries(
-  Object.entries({ rinkeby: {}, mumbai: {} }).map(([network]) => [
-    network,
-    {
-      url: `${process.env[PROVIDER + `_${network.toUpperCase()}_URL`]}${process.env[PROVIDER + "_API_KEY"]}` || "",
-      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    },
-  ])
-);
-
 const config: HardhatUserConfig = {
   // Currently supports only a single solidity version
   solidity: {
-    version: "0.8.9",
+    compilers: [
+      {
+        version: "0.8.9",
+      },
+    ],
   },
-  networks: NETWORKS,
   // create a separate file for mocha config
   mocha: {
     timeout: 50000,
